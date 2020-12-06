4442cadcde1bec5bea7e2b666555e3d49a330b4f
Anna Kaser <kaserann96@gmail.com>
2020-12-06 12:35:56 -0700

    update routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -45,17 +45,17 @@ const thoughtController = {
         Thought.create(body)
             .then(({ _id }) => {
                 return User.findOneAndUpdate(
-                    { _id: params.pizzaId },
-                    { $push: { comments: _id } },
+                    { _id: params.userId },
+                    { $push: { thoughts: _id } },
                     { new: true }
                 );
             })
             .then(dbUserData => {
                 if (!dbUserData) {
-                    res.status(404).json({ messahe: 'No user found with this id!' });
+                    res.status(404).json({ message: 'No user found with this id!' });
                     return;
                 }
-                res.json(dbuserData);
+                res.json(dbUserData);
             })
             .catch(err => res.json(err));
     },
